710a726d5b729a926b677444a4520079184e4912
HTcB-12C7 <sh_cqxzjxcomputer@outlook.com>
2025-04-22 16:07:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // script.js: 前端交互逻辑
+function applyCoupon() {
+  document.getElementById('final-price').textContent = '$99.99';
+  alert('优惠码已使用，价格已减至 $99.99！');
+}
+
+function placeOrder() {
+  window.location.href = 'success.html';
+}
