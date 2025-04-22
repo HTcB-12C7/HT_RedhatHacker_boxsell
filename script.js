51e0af048f5e2da2901a9201e55b87ef2b6b150c
HTcB-12C7 <sh_cqxzjxcomputer@outlook.com>
2025-04-22 16:50:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // script.js: 前端交互逻辑
 function applyCoupon() {
-  document.getElementById('final-price').textContent = '￥78.978';
+  document.getElementById('final-price').textContent = '￥0.000';
   alert('优惠码已使用，价格已减至￥0.000！');
 }
 
